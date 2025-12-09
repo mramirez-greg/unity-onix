3afb95f240129808ee56e81a52af1721bfce7c5e
Mauricio Ramirez <mauricio_ramirez@epam.com>
2025-12-09 12:16:31 -0500

    Se agrego moneda en el mapa y contador de moneda

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,4 +50,12 @@ public class Player : MonoBehaviour
         // esto es para saber siestsi estoy en el piso
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
