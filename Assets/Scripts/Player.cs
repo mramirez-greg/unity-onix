157395e083de30d1e57ba47c33b901ef6faa3cba
Mauricio Ramirez <mauricio_ramirez@epam.com>
2026-01-19 15:41:47 -0500

    Music And Sounds
    Se agregaron los sonidos de la musica del fondo,y el sonido al explotar un barril

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,6 +22,10 @@ public class Player : MonoBehaviour
    private int coins;
    public TMP_Text textCoins;
 
+   public AudioSource audioSource;
+   public AudioClip coinClip;
+   public AudioClip barrelClip;
+
 
 
     void Start()
@@ -59,6 +63,7 @@ public class Player : MonoBehaviour
     {
         if(collision.transform.CompareTag("Coin"))
         {
+            audioSource.PlayOneShot(coinClip);
             Destroy(collision.gameObject);
             coins++;
             textCoins.text = coins.ToString();
@@ -71,6 +76,7 @@ public class Player : MonoBehaviour
 
         if(collision.transform.CompareTag("Barrel"))
         {
+            audioSource.PlayOneShot(barrelClip);
             Vector2 knockbackDir = (rb2D.position- (Vector2)collision.transform.position).normalized;
             rb2D.linearVelocity = Vector2.zero;
             rb2D.AddForce(knockbackDir*3,ForceMode2D.Impulse);
